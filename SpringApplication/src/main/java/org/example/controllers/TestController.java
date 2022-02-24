6736ff559592bb46edfbb1367ae25622ec5d02ed
Lumbril <32036622+Lumbril@users.noreply.github.com>
2022-02-24 20:16:10 +0300

    test controller for html_string

--- SpringApplication/src/main/java/org/example/controllers/TestController.java
+++ SpringApplication/src/main/java/org/example/controllers/TestController.java
@@ -1,7 +1,9 @@
 package org.example.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
@@ -13,5 +15,20 @@ public class TestController {
     @GetMapping("/lorem")
     public String lorem() {
         return "test";
+    }
+
+    @GetMapping(value = "/html_string", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String htmlString() {
+        return "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "Lorem ipsum dolor sit amet!\n" +
+                "</body>\n" +
+                "</html>";
     }
 }
